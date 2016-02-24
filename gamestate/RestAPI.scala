976d68905b6f52935f009a7643150d560e768c06
RawIron <manianology@gmail.com>
2016-02-24 22:25:56 +0100

    loadtest gamestate

---
+++ gamestate/RestAPI.scala
@@ -0,0 +1,65 @@
+package loadtest.gamestate
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import io.gatling.http.Headers.Names._
+
+import scala.concurrent.duration._
+import bootstrap._
+import assertions._
+
+import loadtest.util._
+import loadtest.gamestate.settings._
+
+
+object RestAPI extends Headers
+               with ParseJsonResponse
+{
+  val save_game =
+      http("save_game")
+        .post(Settings.SAVE_GAME_URL)
+        .param("user_id", "${user_id}")
+        .param("skey", "${skey}")
+        .param("version_token", "${version_token}")
+        .param("game_state", "${gamestate}")
+        .headers(headers_3)
+        .check(
+          status.is(200),
+          jsonPath("$.success").is("true"),
+          jsonPath("$.version_token").exists.saveAs("version_token")
+          )
+
+  val load_game =
+      http("load_game")
+        .get(Settings.LOAD_GAME_URL)
+        .queryParam("user_id", "${user_id}")
+        .queryParam("skey", "${skey}")
+        .headers(headers_3)
+        .check(
+          status.is(200),
+          jsonPath("$.success").is("true"),
+          jsonPath("$.version_token").exists.saveAs("version_token"),
+          jsonPath("$.game_content")
+          .transform(
+            gamestateMaybe => {
+              val gamestate: Array[Byte] = gamestateMaybe match {
+                case None => "".getBytes
+                case Some(state) => state.getBytes
+              }
+              Option(MD5_Digest.md5SumString(gamestate))
+            })
+          .is("${gamestate_md5}"))
+
+  val match_version =
+      http("match_version")
+        .post(Settings.MATCH_VERSION_URL)
+        .param("user_id", "${user_id}")
+        .param("skey", "${skey}")
+        .param("version_token", "${version_token}")
+        .headers(headers_3)
+        .check(
+          status.is(200),
+          jsonPath("$.success").is("true")
+          )
+}
---
+++ gamestate/Settings.scala
@@ -0,0 +1,9 @@
+package loadtest.gamestate.settings
+
+
+object Settings
+{
+	val SAVE_GAME_URL = "/game/player/save_game/"
+	val LOAD_GAME_URL = "/game/player/load_game/"
+	val MATCH_VERSION_URL = "/game/player/match_version/"
+}
