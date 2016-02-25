ddb89dc2a4d7dfe0ce4552000fac5eba91f3617d
RawIron <manianology@gmail.com>
2016-02-25 09:20:25 +0100

    loadtest riak

---
+++ riak/GameContent.scala
@@ -0,0 +1,15 @@
+package loadtest.riak.test
+
+
+object TestData {
+	def gamecontent: String = {
+		"""
+		{"content": {
+		  "avatars": [],
+		  "buildings": [],
+      "levels": []
+		  },
+		"success": true}
+		"""
+	}
+}
